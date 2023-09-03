aac62c0b0209a770f1a49ec59f8ea5ce276ac296
Antonio Souza <antonio.eschola@gmail.com>
2023-09-03 08:03:52 -0400

    feat: implement id generation for users

---
+++ internal/entity/user.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"github.com/arferreira/goham/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	passwordHashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       entity.NewID(),
+		Name:     name,
+		Email:    email,
+		Password: string(passwordHashed),
+	}, nil
+}
+
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
