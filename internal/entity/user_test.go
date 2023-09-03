e4a35ff0ef1f610349d642be994b8d9c90ba046c
Antonio Souza <antonio.eschola@gmail.com>
2023-09-03 08:07:50 -0400

    test: add test to create user

---
+++ internal/entity/user_test.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("Antonio", "arfs.antonio@gmail.com", "123456")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "Antonio", user.Name)
+	assert.Equal(t, "arfs.antonio@gmail.com", user.Email)
+}
